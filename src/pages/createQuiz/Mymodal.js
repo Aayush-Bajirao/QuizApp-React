2aab5efa72382567b9e3be5fb79c2bb13d2f145b
Aayush-Bajirao <aayubaji1208@gmail.com>
2024-05-29 02:03:33 +0530

    Modal 1 done..

--- src/pages/createQuiz/Mymodal.js
+++ src/pages/createQuiz/Mymodal.js
@@ -1,6 +1,12 @@
 import "./Mymodal.css";
+import { useState } from "react";
 
 const Mymodal = ({ closeModal }) => {
+  const [isActive, setIsActive] = useState();
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
   return (
     <div>
       <div class="modal-wrapper"></div>
@@ -10,8 +16,26 @@ const Mymodal = ({ closeModal }) => {
 
           <div class="display-flex buttons">
             <p class="quiz-type">Quiz Type</p>
-            <button class="qna-button">Q & A</button>
-            <button class="poll-type-button">Poll Type</button>
+            <button
+              onClick={handleClick}
+              class="qna-button"
+              style={{
+                backgroundColor: isActive ? "#60B84B" : "white",
+                color: isActive ? "white" : "#757575",
+              }}
+            >
+              Q & A
+            </button>
+            <button
+              onClick={handleClick}
+              class="poll-type-button"
+              style={{
+                backgroundColor: isActive ? "white" : "#60B84B",
+                color: isActive ? "#757575" : "white",
+              }}
+            >
+              Poll Type
+            </button>
           </div>
           <div class="action-buttons">
             <button class="cancel" onClick={closeModal}>
